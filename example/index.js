656429677286f4efffdfc32b1e3cffc7639825a0
Taylor Baldwin <taylorbaldwin@gmail.com>
2017-04-08 09:25:02 -0400

    adding some updates to the example

--- example/index.js
+++ example/index.js
@@ -14,12 +14,23 @@ const settings = {
 const gui = new GUI()
 gui.add(settings, 'dampening', 0, 1.0).step(0.01).onChange(setupSprings)
 gui.add(settings, 'stiffness', 0, 1.0).step(0.01).onChange(setupSprings)
+gui.add({ clear }, 'clear')
+
+function clear () {
+  lastValues = []
+}
 
 const canvas = document.querySelector('canvas')
 const ctx = setupCanvasAndGetContext(canvas)
 
 canvas.addEventListener('click', (e) => {
-  lastValues = []
+  if (
+    circle.xSpring.isAtDestination(0.2) &&
+    circle.ySpring.isAtDestination(0.2) &&
+    circle.radiusSpring.isAtDestination(0.2)
+  ) {
+    lastValues = []
+  }
   setPositionAndRadius([e.offsetX, e.offsetY])
 })
 
@@ -62,21 +73,21 @@ function setupCanvasAndGetContext (canvas) {
 function loop () {
   requestAnimationFrame(loop)
   clearRect(ctx, 'rgb(248, 245, 250)')
-  const x = circle.xSpring.tick()
+  const x = circle.xSpring.tick(2)
   const y = circle.ySpring.tick()
   const radius = circle.radiusSpring.tick(2)
   lastValues.push([[x, y], radius])
-  lastValues.forEach(([pos, rad]) => drawCircle(ctx, pos, rad, false))
-  drawCircle(ctx, [x, y], radius)
+  lastValues.forEach(([pos, rad]) => drawCircle(ctx, pos, rad, 'transparent', 'rgba(94, 126, 178, 0.4)'))
+  drawCircle(ctx, [x, y], radius, 'rgb(94, 126, 178)', 'rgba(255, 255, 255, 0.8)')
 }
 
-function drawCircle (ctx, position, radius, shouldFill = true) {
+function drawCircle (ctx, position, radius, fillColor = 'transparent', strokeColor = 'transparent') {
   if (radius < 0) return
   ctx.beginPath()
-  ctx.strokeStyle = 'rgba(94, 126, 178, 0.4)'
-  ctx.fillStyle = 'rgb(94, 126, 178)'
+  ctx.strokeStyle = strokeColor
+  ctx.fillStyle = fillColor
   ctx.arc(position[0], position[1], radius, 0, Math.PI * 2)
-  if (shouldFill) ctx.fill()
+  ctx.fill()
   ctx.stroke()
 }
 
